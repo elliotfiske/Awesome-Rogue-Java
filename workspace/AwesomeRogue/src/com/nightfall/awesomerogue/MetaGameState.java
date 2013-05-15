f39cf265b55ebff3456f01db0ebd5a67edfb13e5
Thomas Steinke <exyphnos@gmail.com>
2013-05-15 00:22:14 -0700

    Fixed flickering (I think) and added shading. PURDY

---
+++ workspace/AwesomeRogue/src/com/nightfall/awesomerogue/MetaGameState.java
@@ -0,0 +1,31 @@
+package com.nightfall.awesomerogue;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class MetaGameState extends GameState {
+
+	public MetaGameState(GamePanel gameCanvas) {
+		super(gameCanvas);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPress(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
